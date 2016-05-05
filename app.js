882a4e5d6ab99c62aa676494617397d5d3c9e58d
Matthew Rosendin <matt.rosendin@gmail.com>
2016-05-04 17:12:06 -0700

    Display SDK data for all audit formats

--- app.js
+++ app.js
@@ -1,29 +1,50 @@
 (function() {
 
   return {
+
+    SDK: {
+      sdk_for_android: {name: 'SDK for Android', image: 'android.png'}, 
+      sdk_for_ios: {name: 'SDK for iOS', image: 'ios.png'}, 
+      unity_1_for_android: {name: 'Unity Plugin (v1.0, v.1.1) for Android', image: 'unity.png'},
+      unity_1_for_ios: {name: 'Unity Plugin (v1.0, v1.1) for iOS', image: 'unity.png'},
+      unity_1_4_for_android: {name: 'Unity Plugin (v1.4) for Android', image: 'unity.png'},
+      unity_1_4_for_ios: {name: 'Unity Plugin (v1.4) for iOS', image: 'unity.png'}
+    },
+
+    // REQUESTS ========================================================================
+
     requests: {
+
       fetchTicketAudits: function () {
         return {
           url: '/api/v2/tickets/' + this.ticket().id() + '/audits.json',
           type: 'GET'
         };
       },
+
       getLocationWeather: function (lat, lon) {
         return {
           url: 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=8a64f5ac55f62c14e1d5788f6acbfe52'
         };
       },
-      // getLatestSDKRelease: function (OS) {
-      //   return {
-      //     url: 'http://developer.zendesk.com/embeddables/docs/' + OS + '/migration',
-      //     type: 'GET',
-      //     dataType: 'html'
-      //   }
-      // }
+
+      /*********************************************************************************
+      getLatestSDKRelease: function (OS) {
+        return {
+          url: 'https://api.github.com/repos/zendesk/zendesk_sdk_ios/releases/latest',
+          type: 'GET',
+          dataType: 'application/json'
+        }
+      }
+      *********************************************************************************/
+
     },
+
+    // EVENTS ==========================================================================
+
     events: {
       'app.activated':'init',
-      'fetchTicketAudits.done':'extractSDKProperties',
+      'fetchTicketAudits.done':'findSDKAudit',
       'fetchTicketAudits.fail':'showErrorMessage',
       'iframe.registered':'processSDKProperties',
       // 'getLatestSDKRelease.done':'showLatestRelease',
@@ -32,86 +53,100 @@
       'getLocationWeather.fail':'showErrorMessage'
     },
 
+    // EVENT HANDLERS ==================================================================
+
     init: function() {
       this.ajax('fetchTicketAudits');
     },
 
-    extractSDKProperties: function (data) {
+    findSDKAudit: function (data) {
       var audits = data.audits;
-      this.mobile_sdk_ticket = false;
 
       // Check for a ticket audit completed through the mobile_sdk channel
-      _.each(audits, function (audit) {
-        if (audit.via.channel == 'mobile_sdk') {
-          if (audit.metadata.custom.hasOwnProperty('sdk')) {
-            this.mobile_sdk_ticket = true;
-            this.client = audit.metadata.system.client;
-            this.ip_address = audit.metadata.system.ip_address;
-            this.latitude = audit.metadata.system.latitude;
-            this.longitude = audit.metadata.system.longitude;
-            this.user_location = audit.metadata.system.location;
-            this.device_model = audit.metadata.custom.sdk.device_model;
-            this.device_os = audit.metadata.custom.sdk.device_os;
-            this.device_total_memory = audit.metadata.custom.sdk.device_total_memory;
-            this.device_storage = audit.metadata.custom.sdk.device_storage;
-            this.device_battery = audit.metadata.custom.sdk.device_battery;
-            this.trusted = audit.metadata.trusted;
-          }
-        }
+      this.sdk_audit = _.find(audits, function (audit) {
+        return audit.via.channel === 'mobile_sdk'; 
       }, this);
 
-      this.loadView();
+      typeof this.sdk_audit !== 'undefined' ? this.parseClient(this.sdk_audit) : this.showErrorMessage();
+    },
+
+    parseClient: function () {
+      // Prepare the client information for template
+      var split = this.sdk_audit.metadata.system.client.split(' ');
+
+      if (split.length === 3) {
+        // SDK for iOS or SDK for Android
+        if (split[1].indexOf('iOS') > -1) {
+          this.sdk = this.SDK['sdk_for_ios'];
+          this.os = 'iOS';
+        } 
+        if (split[1].indexOf('Android') > -1) {
+          this.sdk = this.SDK['sdk_for_android'];
+          this.os = 'Android';
+        }
+      } else if (split.length === 4) {
+        // Unity Plugin (v1.0, v1.1)
+        if (split[3].indexOf('iOS') > -1) {
+          this.sdk = this.SDK['unity_1_for_ios'];
+          this.os = 'iOS';
+        }
+        if (split[3].indexOf('Android') > -1) {
+          this.sdk = this.SDK['unity_1_for_android'];
+          this.os = 'Android';
+        }
+        // Unity Plugin (v1.4)
+        if (split[3].indexOf('Unity') > -1) {
+          if (split[1].indexOf('iOS') > -1) {
+            this.sdk = this.SDK['unity_1_4_for_ios'];
+            this.os = 'iOS';
+          } else {
+            this.sdk = this.SDK['unity_1_4_for_android'];
+            this.os = 'Android';
+          }
+        }
+      }
+
+      this.loadView()
     },
 
     loadView: function () {
-      if (this.mobile_sdk_ticket === true) {
-        this.switchTo('main', { client: this.client, ip_address: this.ip_address, user_location: this.user_location, device_model: this.device_model, device_os: this.device_os,
-        device_total_memory: this.device_total_memory, device_storage: this.device_storage, device_battery: this.device_battery, trusted: this.trusted });
-      } else {
-        console.log('This ticket doesn\'t have any mobile SDK ticket audits.');
-      }
+      this.system = this.sdk_audit.metadata.system;
+      if (this.sdk_audit.metadata.custom.hasOwnProperty('sdk')) { this.device = this.sdk_audit.metadata.custom.sdk; }
+
+      // Display the core template that displays common metadata across all SDKs (sdk_audit.metadata.system)
+      this.switchTo('system', {system: this.system, sdk: this.sdk.name });
+
+      // Use renderTemplate(templateName, data) to load metadata into template then insert into system.hdbs
+      var isUnity, template;
+      this.sdk['image'] == 'unity.png' ? isUnity = true : isUnity = false;
+      this.os == 'Android' ? template = 'android-device' : template = 'ios-device';
+      var deviceTemplate = this.renderTemplate(template, { device: this.device, sdk_image: this.sdk.image, isUnity: isUnity });
+      this.$('#device').html(deviceTemplate);
     },
 
     processSDKProperties: function () {
-      // Parse the client for the device OS and set the correct device OS image
-      var clientData = this.client.split(' ');
-      var SDKClient = clientData[0];
-      var clientOS = clientData[1];
-      var OSImageSource;
-      var OS;
-      if (clientOS.indexOf('iOS') > -1) {
-        // Use iOS image
-        OSImageSource = this.assetURL('ios.png');
-        OS = 'android';
-      } else if (clientOS.indexOf('Android') > -1) {
-        // Use Android image
-        OSImageSource = this.assetURL('android.png');
-        OS = 'ios';
+      // Only iOS devices can display battery percentage
+      if (this.os == 'iOS') {
+        // Determine which battery image to use
+        var batteryImageSource;
+        if (this.device_battery >= 75) {
+          batteryImageSource = this.assetURL('battery-4.png');
+        } else if (this.device_battery >= 50 && this.device_battery < 75) {
+          batteryImageSource = this.assetURL('battery-3.png');
+        } else if (this.device_battery >= 25 && this.device_battery < 50) {
+          batteryImageSource = this.assetURL('battery-2.png');
+        } else if (this.device_battery >= 5 && this.device_battery < 25) {
+          batteryImageSource = this.assetURL('battery-1.png');
+        } else {
+          batteryImageSource = this.assetURL('battery-0.png');
+        }
+        this.$('div#device_battery img').attr('src', batteryImageSource);
       }
-      this.$('#device_specs img').attr('src', OSImageSource);
-      this.$('#device_os')[0].textContent = clientOS;
-      this.$('#client')[0].textContent = SDKClient;
-
-      // Determine which battery image to use
-      var batteryImageSource;
-      if (this.device_battery >= 75) {
-        batteryImageSource = this.assetURL('battery-4.png');
-      } else if (this.device_battery >= 50 && this.device_battery < 75) {
-        batteryImageSource = this.assetURL('battery-3.png');
-      } else if (this.device_battery >= 25 && this.device_battery < 50) {
-        batteryImageSource = this.assetURL('battery-2.png');
-      } else if (this.device_battery >= 5 && this.device_battery < 25) {
-        batteryImageSource = this.assetURL('battery-1.png');
-      } else {
-        batteryImageSource = this.assetURL('battery-0.png');
-      }
-
-      this.$('div#device_battery img').attr('src', batteryImageSource);
 
       // Send coordinates to Google map
-      this.postMessage('mapCoordinates', { latitude: this.latitude, longitude: this.longitude });
+      this.postMessage('mapCoordinates', { latitude: this.system.latitude, longitude: this.system.longitude });
 
-      // Get latest Zendesk SDK version from developer.zendesk.com
+      // Get latest Zendesk SDK version from developer.zendesk.com or GitHub
       // this.ajax('getLatestSDKRelease', OS);
 
       // Get weather from location
@@ -120,26 +155,27 @@
     },
 
     // showLatestRelease: function (response) {
-    //   console.log(response);
-    //   console.log(response.indexOf('<td>'));
+
     // },
 
     showLocationWeather: function (data) {
-      if (data.weather[0].main == "Rain") {
-        this.$('div#weather img').attr('src', this.assetURL('rain.png'));
-      } else if (data.weather[0].main == "Clouds") {
-        this.$('div#weather img').attr('src', this.assetURL('clouds.png'));
-      } else if (data.weather[0].main == "Clear") {
-        this.$('div#weather img').attr('src', this.assetURL('clear.png'));
-      } else if (data.weather[0].main == "Mist") {
-        this.$('div#weather img').attr('src', this.assetURL('mist.png'));
-      } else {
-        this.$('div#weather').textContent = data.weather[0].main;
+      if (data.weather) {
+        if (data.weather[0].main == "Rain") {
+          this.$('div#weather img').attr('src', this.assetURL('rain.png'));
+        } else if (data.weather[0].main == "Clouds") {
+          this.$('div#weather img').attr('src', this.assetURL('clouds.png'));
+        } else if (data.weather[0].main == "Clear") {
+          this.$('div#weather img').attr('src', this.assetURL('clear.png'));
+        } else if (data.weather[0].main == "Mist") {
+          this.$('div#weather img').attr('src', this.assetURL('mist.png'));
+        } else {
+          this.$('div#weather').textContent = data.weather[0].main;
+        }
       }
     },
 
     showErrorMessage: function () {
-      this.switchTo('error', { message: 'There was an error.' });
+      this.switchTo('error', 'message: There was an error or there were no mobile audits.');
     }
 
   };
