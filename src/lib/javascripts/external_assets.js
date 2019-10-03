38c4f678b31643e8171a780ee16a02f907f7d4dd
sostopher <csos@zendesk.com>
2019-10-03 19:41:02 +1000

    Move lib under src

---
+++ src/lib/javascripts/external_assets.js
@@ -0,0 +1,7 @@
+module.exports = {
+  css: ["https://cdn.jsdelivr.net/combine/npm/@zendeskgarden/css-bedrock@7.0.27,npm/@zendeskgarden/css-utilities@4.4.1,npm/@zendeskgarden/css-buttons@7.0.12,npm/@zendeskgarden/css-forms@7.0.13,npm/@zendeskgarden/css-tags@5.1.8"],
+  js: [
+    "https://cdn.jsdelivr.net/g/lodash@2.4.2(lodash.underscore.min.js),handlebarsjs@1.3.0,jquery@2.2.4,momentjs@2.9.0,bootstrap@2.3.2",
+    "https://static.zdassets.com/zendesk_app_framework_sdk/2.0/zaf_sdk.min.js"
+  ]
+};
